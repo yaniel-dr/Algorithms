3530b64812e9f9c2a1ff6addc74095eec1992732
Yaniel Diaz Rubio <yaniel@Yaniels-MacBook-Air.local>
2020-11-12 18:09:08 -0500

    Implement IntShuffledRepresentation Algorithm

---
+++ Algorithms.Implementation/IntShuffledRepresentation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Algorithms.Implementation
+{
+    public class IntShuffledRepresentation
+    {
+        public int Solution(int value)
+        {
+            int numberOfDigits = Convert.ToInt32(Math.Floor(Math.Log10(value))) + 1;
+            if(numberOfDigits == 1 || numberOfDigits == 2) return value;
+            
+            int result = 0;
+            var numberOfIterations = numberOfDigits / 2;
+            for (int i = 0; i < numberOfIterations; i++)
+            {
+                var firstDigitPosition = numberOfDigits - i;
+                var secondDigitPosition = i + 1;
+                var newFirstDigitPosition = numberOfDigits - 2*i;
+                var newSecondDigitPosition = numberOfDigits - 2*i - 1;
+                result += 
+                    GetNumberForPosition(GetDigitInPosition(value, firstDigitPosition), newFirstDigitPosition ) + 
+                    GetNumberForPosition(GetDigitInPosition(value, secondDigitPosition), newSecondDigitPosition);
+            } 
+
+            var isEvenNumber = numberOfDigits % 2 == 0;
+            if(isEvenNumber) return result;
+
+            var middleDigitPosition = numberOfIterations + 1;
+            result += GetNumberForPosition(GetDigitInPosition(value, middleDigitPosition), 1);
+
+            return result;   
+        }
+
+        private int GetDigitInPosition(int number, int position)
+        {
+            return (number / (int)Math.Pow(10, position - 1)) % 10;
+        }
+        private int GetNumberForPosition(int number, int position)
+        {
+            return number * (int)Math.Pow(10, position - 1);
+        }
+    }
+}
---
+++ Algorithms.Test/IntShuffledRepresentationTests.cs
@@ -0,0 +1,21 @@
+using Algorithms.Implementation;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Test
+{
+    public class IntShuffledRepresentationTests
+    {
+        IntShuffledRepresentation sut = new IntShuffledRepresentation();
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(13, 13)]
+        [InlineData(130, 103)]
+        [InlineData(123456, 162534)]
+        public void Should_Shuffle( int value, int solution)
+        {
+            sut.Solution(value).Should().Be(solution);
+        }
+    }
+}
